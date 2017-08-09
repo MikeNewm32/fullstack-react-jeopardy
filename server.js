e69c943acd8d780739ffd39a3f5b9e7a18734278
MikeNewm32 <michaelnewman85@hotmail.com>
2017-08-09 10:50:59 -0400

    first inclass commit

---
+++ server.js
@@ -0,0 +1,29 @@
+require("dotenv").config();
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const app = express();
+
+mongoose.Promise = global.Promise;
+
+mongoose.connect(process.env.MONGODB_URI);
+const connection = mongoose.connection;
+
+connection.on('connected', () => {
+    console.log("Mongoose Connected Successfully");
+});
+
+connection.on('error', (err) => {
+    console.log("Mongoose connection error: " + err);
+});
+
+app.use(bodyParser.json());
+
+app.get("/", (req, res) => {
+    res.send("Hello world");
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log("App is listening on : " + PORT);
+});
