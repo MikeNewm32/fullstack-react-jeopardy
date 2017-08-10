829414c7152da2624aab1a7d723c56878dc03df7
MikeNewm32 <michaelnewman85@hotmail.com>
2017-08-10 14:15:55 -0400

    last update for jeopardy

--- controllers/category.js
+++ controllers/category.js
@@ -3,24 +3,31 @@ const Category = require("../models/category");
 const Question = require("../models/question");
 const router = express.Router();
 
+const express = require("express");
+const Category = require("../models/category");
+const Question = require("../models/question");
+const router = express.Router();
+
 router.get("/", (req, res) => {
-    Category.find().then((categories) => {
-        res.json(categories);
-    });
+  Category.find().then((categories) => {
+    res.json(categories);
+  });
 });
 
 router.post("/", (req, res) => {
-    const newCategory = new Category();
-    newCategory.name = req.body.category.name;
+  const newCategory = new Category();
+  console.log(req.body);
+  newCategory.name = req.body.category.name;
+  
+  const newQuestions = req.body.category.questions.map((question) => {
+    return new Question(question);
+  });
 
-    const newQuestions = req.body.category.questions.map((question) => {
-        return new Question(question);
-    });
-    newCategory.questions = newQuestions;
+  newCategory.questions = newQuestions;
 
-    newCategory.save().then((category) => {
-        res.json(category);
-    }).catch(err => console.log(err));
-});
+  newCategory.save().then((category) => {
+    res.json(category);
+  }).catch(err => console.log(err));
+})
 
 module.exports = router;
--- controllers/game.js
+++ controllers/game.js
@@ -1,17 +1,42 @@
 const express = require("express");
 const Game = require("../models/game");
+const Category = require("../models/category");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-    Game.find().then((games) => {
-        res.json(games);
-    })
+  Game.find().then((games) => {
+    res.json(games);
+  });
+});
+
+router.post("/", (req, res) => {
+  const newGame = new Game();
+  newGame.user = req.body.user;
+  newGame.points = 0;
+  newGame.categories = []
+
+  Category.find().then((categories) => {
+    for (var i = 0; i < 6; i ++){
+      const randomNumber = Math.floor(Math.random() * categories.length);
+      const randomCategory = categories[randomNumber];
+      newGame.categories.push(randomCategory);
+    }
+    return newGame.save();
+  }).then((game) => {
+    return res.json(game);
+  });
 });
 
 router.get("/:id", (req, res) => {
-    Game.findById(req.params.id).then((game) => {
-        res.json(game);
-    })
+  Game.findById(req.params.id).then((game) => {
+    res.json(game);
+  })
 })
+
+router.put("/:id", (req, res) => {
+  Game.findByIdAndUpdate(req.params.id, {points: req.body.points}).then((game) =>{
+    res.json(game);
+  })
+});
 
 module.exports = router;
